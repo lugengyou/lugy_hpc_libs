2518e30bd33519149ec1fac0b34726a40b4ca1a1
lugy <lugengyou@github.com>
2024-09-20 10:14:04 +0800

    [NEW] 规范 reduce 算子代码开发

--- src/cuda/reduce.cu
+++ src/cuda/reduce.cu
@@ -2,7 +2,7 @@
  * @Author: lugy lugengyou@github.com
  * @Date: 2024-09-18 15:32:31
  * @FilePath: /lugy_hpc_libs/src/cuda/reduce.cu
- * @LastEditTime: 2024-09-20 00:12:19
+ * @LastEditTime: 2024-09-20 10:10:24
  * @Description: reduce.cu
  */
 #include "reduce.h"
@@ -15,7 +15,7 @@
  * @description: 单 block 共享内存规约
  * @return {*}
  */
-__global__ void reduce_kernel_v0(int *d_a, int *d_b, int n) {
+__global__ static void reduce_kernel_v0(int *d_a, int *d_b, int n) {
     __shared__ int sdata[256];
 
     const uint32_t tid = threadIdx.x;
@@ -48,7 +48,7 @@ __global__ void reduce_kernel_v0(int *d_a, int *d_b, int n) {
  * @description: 单 block 共享内存规约, block 内剩余一个 warp 使用 warp shuffle 规约
  * @return {*}
  */
-__global__ void reduce_kernel_v1(int *d_a, int *d_b, int n) {
+__global__ static void reduce_kernel_v1(int *d_a, int *d_b, int n) {
     __shared__ int sdata[256];
 
     const uint32_t tid = threadIdx.x;
@@ -85,26 +85,26 @@ __global__ void reduce_kernel_v1(int *d_a, int *d_b, int n) {
 
 void lugy::reduce_cuda(int *a, int *b, int n, int version) {
     int *d_a, *d_b;
+    
     cudaMalloc((void**)&d_a, n * sizeof(int));
     cudaMalloc((void**)&d_b, sizeof(int));
     cudaMemcpy(d_a, a, n * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_b, b, sizeof(int), cudaMemcpyHostToDevice);
 
-    int blockSize = 256;
-    switch (version)
-    {
-        case 0:
-            printf("cuda in reduce_kernel_v0.\n");
-            reduce_kernel_v0<<<1, blockSize, blockSize * sizeof(int)>>>(d_a, d_b, n);
-            break;
-        case 1:
-            printf("cuda in reduce_kernel_v1.\n");
-            reduce_kernel_v1<<<1, blockSize, blockSize * sizeof(int)>>>(d_a, d_b, n);
-            break;
+    if (version == 0){        
+        printf("cuda in reduce_kernel_v0.\n");
+        dim3 block(256);
+        reduce_kernel_v0<<<1, block, block.x * sizeof(int)>>>(d_a, d_b, n);
+    }
+    else if (version == 1){
+        printf("cuda in reduce_kernel_v1.\n");
+        dim3 block(256);
+        reduce_kernel_v1<<<1, block, block.x * sizeof(int)>>>(d_a, d_b, n);
+    }
+    else {
+        printf("version not found\n");
+    }  
 
-        default:
-            break;
-    }    
     cudaMemcpy(b, d_b, sizeof(int), cudaMemcpyDeviceToHost);
     
     cudaFree(d_a);
