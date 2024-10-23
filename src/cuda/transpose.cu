815c9167ee1e29fc456f70afbc4e4d35b8c7efb3
gengyou.lu <1770591868@qq.com>
2024-10-23 15:38:25 +0800

    [NEW] 修改共享内存转置核函数

--- src/cuda/transpose.cu
+++ src/cuda/transpose.cu
@@ -2,7 +2,7 @@
  * @Author: lugy lugengyou@github.com
  * @Date: 2024-09-23 23:31:04
  * @FilePath: /lugy_hpc_libs/src/cuda/transpose.cu
- * @LastEditTime: 2024-10-22 20:22:08
+ * @LastEditTime: 2024-10-23 15:36:42
  * @Description: transpose.cu
  */
 #include "transpose.h"
@@ -20,20 +20,32 @@ static __global__ void transpose_kernel_v0(int *a, int *b, int srcN, int srcM) {
 }
 
 static __global__ void transpose_kernel_v1(int *a, int *b, int srcN, int srcM) {
-    __shared__ int sdata[16][32];
+    __shared__ int sdata[32][32];
 
     size_t y = threadIdx.y + blockIdx.y * blockDim.y; // 行
     size_t x = threadIdx.x + blockIdx.x * blockDim.x; // 列
 
-    if (y < srcN && x < srcM) {
-        // 原始数据索引，转置后的目标数据索引，已经做了转置操作，所以共享内存不需要索引转置
-        size_t src_addr = y * srcM + x;
-        size_t dst_addr = x * srcN + y;
+    size_t src_addr = y * srcM + x;
+    
+    size_t share_idx = threadIdx.y * blockDim.x + threadIdx.x;
 
+    // 计算共享内存转置对应的行列
+    size_t row = share_idx / blockDim.y;
+    size_t col = share_idx % blockDim.y;
+
+    // 转置后的目标矩阵行列
+    size_t trans_y = blockIdx.x * blockDim.x + row;
+    size_t trans_x = blockIdx.y * blockDim.y + col; // trans_x 是连续增加的
+
+    size_t dst_addr = trans_y * srcN + trans_x;
+
+    if (y < srcN && x < srcM) {
+        // 加载数据到共享内存
         sdata[threadIdx.y][threadIdx.x] = a[src_addr];
         __syncthreads();
 
-        b[dst_addr] = sdata[threadIdx.y][threadIdx.x];        
+        // 共享内存按列连续读取，转置按行连续存储     
+        b[dst_addr] = sdata[col][row]; 
     }
 }
 
@@ -54,7 +66,7 @@ void lugy::transpose_cuda(int *a, int *b, int srcN, int srcM, int version) {
     }
     else if (version == 1) {
         printf("cuda in transpose_kernel_v1\n");
-        dim3 block(32, 16);
+        dim3 block(32, 32);
         dim3 grid((srcM + block.x - 1) / block.x, (srcN + block.y - 1) / block.y);
         transpose_kernel_v1<<<grid, block>>>(d_a, d_b, srcN, srcM);
     }
